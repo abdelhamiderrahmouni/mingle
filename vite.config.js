f9b3e545aca4bed7fe6105433bad8a02dc050177
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-11-16 05:06:50 +0100

    Transferred Mingle configs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue';
+import path from 'path'
+import findMingles from './resources/js/mingle/autoImport.js'
+const mingles = findMingles('resources/js')
+// Optional: Output the mingles to the console, for a visual check
+console.log('Auto-importing mingles:', mingles)
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                ...mingles,
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/**',
+                'resources/js/**',
+            ],
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            "@mingle": path.resolve(__dirname, "/resources/js/mingle"),
+        },
+    },
 });
